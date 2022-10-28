89db235804db2f078f20f1bc50f47c75d9aabd1e
Carlos Delgado III <54150248+Carlos-Delgado23@users.noreply.github.com>
2022-10-28 12:48:31 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 // Sections
 import Landing from '@sections/Landing'
-import RSVP from '@sections/RSVP'
-// import Registry from '@sections/Registry'
+// import RSVP from '@sections/RSVP'
+import Registry from '@sections/Registry'
 import Details from '@sections/Details'
 import Answers from '@sections/Answers'
 import Photos from '@sections/Photos'
